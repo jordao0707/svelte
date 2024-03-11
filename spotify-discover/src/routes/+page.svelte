b45d27a0dd08c1138cbc89aa85ca8417276cf64d
Jordão Rodrigues Dantas <jordao.dantas07@gmail.com>
2024-03-10 22:25:08 -0300

    Inicialização do projeto

---
+++ spotify-discover/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="typescript">
+	let counter = 0;
+	function increment() {
+		counter ++;
+		// fetch('https://jsonplaceholder.typicode.com/posts/1')
+		// 	.then((response) => response.json())
+		// 	.then((json) => console.log(json));
+	}
+</script>
+
+<h1>Welcome to Spotify Discover</h1>
+<button on:click={increment}>{counter}</button>
+
+<style>
+    h1{
+        font-family: Arial, Helvetica, sans-serif;
+    } 
+
+	button { 
+        width: 8rem;
+        
+        padding: 1rem;
+        
+        border: none;
+        border-radius: 1rem;
+		
+        background-color: rgb(54, 134, 108);
+
+        &:hover{
+            background-color: rgb(59, 121, 100);
+        }
+	}
+</style>
